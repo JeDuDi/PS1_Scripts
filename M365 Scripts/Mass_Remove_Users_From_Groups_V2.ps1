771cd626f4972e4e62c28e1663ff4c6c05c0769d
JeDuDi <jesse.d.dickens@pm.me>
2023-07-12 15:02:41 -0700

    Update Mass_Remove_Users_From_Groups_V2.ps1

--- M365 Scripts/Mass_Remove_Users_From_Groups_V2.ps1
+++ M365 Scripts/Mass_Remove_Users_From_Groups_V2.ps1
@@ -5,6 +5,10 @@
     .NOTES
         Requires Remove_User_All_GroupsV2.ps1 to be in the same directory
         https://github.com/michevnew/PowerShell/blob/master/Remove_User_All_GroupsV2.ps1
+        You will also need to run the following:
+        Install-Module Microsoft.Graph
+        Import-Module Microsoft.Graph.Users
+        Import-Module Microsoft.Graph.Groups
 
 #>
 
@@ -20,7 +24,7 @@ Import-CSV -Path "C:\temp\HRV_MO-Users-export.csv" | ForEach-Object {
     #Output the current UPN to the terminal
     $upn
          
-    #Assign variables from the CSV file, these are mapped based off of column headers in the CSV file
+    #Execute the removal script with the targeted UPN, see the article in above notes for extra switches to add
     .\Remove_User_All_GroupsV2.ps1 -Identity $upn -IncludeAADSecurityGroups -IncludeOffice365Groups
 
 }
