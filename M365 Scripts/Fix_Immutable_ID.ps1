9f92b28bc357789dbe356e3ebc5ccfdb535afb61
JeDuDi <jesse.d.dickens@pm.me>
2023-09-01 13:33:12 -0700

    Update Fix_Immutable_ID.ps1

--- M365 Scripts/Fix_Immutable_ID.ps1
+++ M365 Scripts/Fix_Immutable_ID.ps1
@@ -1,40 +1,38 @@
-﻿Connect-MsolService
+﻿<#
 
-$var = (Get-MsolUser -UserPrincipalName "Dan.Canfield@andrews-cooper.com").ImmutableID
+    .PURPOSE
+        The purpose of the script is to audit and update the ImmutableID field in M365 to fix duplicated account sync issues.
+    .NOTES
+        You will need the MSOL PowerShell module installed to run these commands.
+        Be careful not to lose the ObjectID and ImmutableID data during your audit as these cannot be recovered after deletion.
 
-Write-Host($var)
+#>
 
-Set-MsolUser -UserPrincipalName "Dan.Canfield@andrews-cooper.com" -ImmutableID "EQHDJsAWg0CXIlDBdKpvMg=="
+#Connect to the necessary service for this script
+Connect-MsolService
 
-Get-MsolUser -UserPrincipalName "Dan.Canfield@andrews-cooper.com" | Format-List
+#Get the ImmutableID for both accounts of the duplicated user
 
-#Steve - Mac
-#vJMrSv90pUiadth81TG6bw==
+#Log the Cloud-only Immutable ID (probably blank): EQHDJsAWg0CXIlDBdKpvMg==
+(Get-MsolUser -UserPrincipalName "clouduser@domain.com").ImmutableID
 
-#ACarcia - LO
-#UyxxTQalsUaCC2TFBQeNdA==
+#Log the ADSynced Account Immutable ID (to be assigned to the cloud-only account in another step): EQHDJsAWg0CXIlDBdKpvMg==
+(Get-MsolUser -UserPrincipalName "synceduser@domain.com").ImmutableID
 
-#Sami - Corp
-#CNoGhP9KBkKWVF7EO48ktQ==
+<#
 
-#act.local/Andrews-Cooper Corporate/Users/Staff/Barbara Duong
-#PJJOC1jOlkyk1FiV96Qo1w==
+    Now that you have the Immutable IDs logged for future use, it's time to move the local AD account into an OU that doesn't sync with AzureAD. Usually a Disabled Users OU.
+    Run a Delta Sync on the AD server. Give it about 30 seconds to complete.
+    Now check the deleted users in Azure AD. You should see the duplicate account of the ADSynced user in the Deleted Users menu.
+    Granted this account is unlicensed with no data, permanently delete this account in Azure AD. Give it about 30 seconds and refresh the page.
+    Once the duplicate account is no longer showing in Azure AD, proceed to the next step.
 
-#act.local/Andrews-Cooper Corporate/Users/Staff/Shawn Daniel
-#+O/m/YsWvEeyJJRqIdX6jQ==
+#>
 
-#act.local/Andrews-Cooper Lake Oswego/Users/Staff/Tyler Kenney
-#avQixJQJt0yPrqxkoRzRnA==
+#Now you will take the ImmutableID from the recently deleted ADSynced account and assign it to the cloud-only account
+Set-MsolUser -UserPrincipalName "clouduser@domain.com" -ImmutableID "EQHDJsAWg0CXIlDBdKpvMg=="
 
-
-
-#Dan
-#EQHDJsAWg0CXIlDBdKpvMg==
-
-#Was Xtreme
-#WHcqKmFFoEebBM85VcRIig==
-
-
+#Conversion tool for Hex, Base64, and GUID needed in rare cases
 $hexstring = "11 01 C3 26 C0 16 83 40 97 22 50 C1 74 AA 6F 32"     # <- set your own Hex string here
 $base64 = [system.convert]::ToBase64String([byte[]] (-split (($hexstring -replace " ", "") -replace '..', '0x$& ')))
 $base64
@@ -42,35 +40,3 @@ $base64
 $hexstring = "11 01 C3 26 C0 16 83 40 97 22 50 C1 74 AA 6F 32"     # <- set your own Hex string here
 $guid = [GUID]([byte[]] (-split (($hexstring -replace " ", "") -replace '..', '0x$& ')))
 $guid
-
-
-
-Connect-AzureAD
-
-$usr = Get-AzureADUser -ObjectID "teamlogicit@andrews-cooper.com"
-
-$usr.DisplayName
-
-$usr.MailNickName = ""
-$usr.OnPremisesSecurityIdentifier = ""
-
-Get-AzureADUser -ObjectId "dan.canfield@andrews-cooper.com"
-
-
-Get-AzureADObjectByObjectId -ObjectIds 8b2820c1-fbce-4cc3-819b-d5c8cba996be
-
-
-
-
-#774857ce-c9d5-47dc-b033-b02fe352312d
-#b3ba2b4d-5298-4efe-87a2-39647c7d2d9c
-#8b2820c1-fbce-4cc3-819b-d5c8cba996be
-#05a83803-fa0b-4599-953b-24ea9e81adc5
-#0801e996-befb-4afb-8357-4b9297762692
-#55bbb6ac-ab37-464d-8d9b-85081bc391c7
-
-Get-AzureADUser -ObjectId e008687c-ba87-4497-9bc2-9593edca634e | Format-List
-
-Set-MsolUser -UserPrincipalName "aaron.chritton@andrews-cooper.com" -ImmutableID ""
-
- S-1-5-21-3143605458-3659537772-2962101204-1154
